9269fb786b4def040691de6c51ea2f013ccb0655
Sibeliu <jonah@opus73.org>
2025-03-03 14:04:10 +0700

    following along

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Poppins } from 'next/font/google';
+
+ 
+export const inter = Poppins({ weight: "300", subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: "700", subsets: ['latin'] });
